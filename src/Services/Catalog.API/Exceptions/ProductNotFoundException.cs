dd79009ae518e161eab122b308e026358dca093c
Hvuthai <hoangvuthai.bl1@gmail.com>
2024-10-06 19:21:43 +0700

    CatalogCRUD API and config gateway

---
+++ src/Services/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : NotFoundException
+{
+    public ProductNotFoundException(string productId) : base("Product", productId)
+    {
+    }
+}
---
+++ src/Services/Catalog.API/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Catalog.API.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public string CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; } = default!;
+        [Required]
+        public string Type { get; set; } = default!;
+        public string CreatedBy { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = default!;
+        public string UpdatedBy { get; set; } = default!;
+        public DateTime UpdatedAt { get; set; } = default!;
+        [Required]
+        public bool Status { get; set; } = default!;
+        public virtual List<ProductCategory> ProductCategories { get; set; } = default!;
+    }
+}
