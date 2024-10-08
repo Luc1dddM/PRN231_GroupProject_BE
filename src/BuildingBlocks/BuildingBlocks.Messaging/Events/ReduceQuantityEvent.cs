72d003f6365ebfd4e441bac1e52b47fc49ed96d9
Hvuthai <hoangvuthai.bl1@gmail.com>
2024-10-08 14:39:21 +0700

    Add BuildingBlocks.Messaging (RabbitMQ)

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/ReduceQuantityEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Messaging.Events
+{
+    public class ReduceQuantityEvent
+    {
+        public string color { get; set; }
+        public string productId { get; set; }
+        public bool status { get; set; }
+        public int quantity { get; set; }
+        public string user { get; set; }
+        public bool IsCancel {  get; set; }
+    }
+}
